238cef96be36bc352d868fa356a8bdc59d878c53
Yauheni Kisialiou <theFree.online93@gmail.com>
2025-02-24 13:17:50 +0300

    test conf

--- src/test/java/Training.java
+++ src/test/java/Training.java
@@ -11,7 +11,6 @@ public class Training {
     private static final String serverUrl = "jdbc:mysql://fortvision.cluster-ro-c1pinvi6ti7x.eu-west-1.rds.amazonaws.com:3306/";
     private static final String user = "fortvision_admin";
     private static final String password = "aYLm3Z2am16jjKSqq501HahnBjKd0h";
-    private static final String query = "SELECT * FROM dashboard.account_details ad";
     private static final SelenideElement searchInput = $x("//dev['']");
     private static final SelenideElement emailField = $x("//input[@id='email']");
     private static final SelenideElement passwordField = $x("//input[@id='password']");
